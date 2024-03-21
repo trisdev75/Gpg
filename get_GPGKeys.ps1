764f68e03e539f3f6825816cb20abd09b2d4c0c6
Tristan <55245589+trisdev75@users.noreply.github.com>
2024-03-21 14:00:46 +0100

    Add files via upload

---
+++ get_GPGKeys.ps1
@@ -0,0 +1,49 @@
+﻿# List GPG keys and format them into a table
+$gpgKeys = @()
+$gpgKeysList = gpg --list-keys --with-colons
+
+foreach ($key in $gpgKeysList) {
+    if ($key -match "^pub:") {
+        $fields = $key.Split(':')
+        $keyID = $fields[4]
+        $gpgKeys += [PSCustomObject]@{
+            KeyID = $keyID
+        }
+    }
+}
+
+$gpgkey =@()
+foreach($key in $gpgKeys)
+{
+    $Id = $key.KeyId
+
+    $GPGKeytarget = gpg --list-keys $Id
+
+    foreach($line in $GPGKeytarget)
+    {
+        if ($line -match '^pub\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)$') {
+            $keyType = $Matches[1]
+            $keyCreation= $Matches[2]
+            $keyId = $Matches[3]
+            $Expiration = $Matches[6]
+
+            $gpgKey += [PSCustomObject]@{
+            KeyID = $Id
+            keyType = $keyType
+            KeyCreation = $keyCreation
+            Expiration = $Expiration.Replace(']','')
+        }
+        
+        }
+
+    
+    }
+}
+ 
+# Output the keys in a table format
+$gpgKey | Format-Table -AutoSize
+
+
+
+
+
